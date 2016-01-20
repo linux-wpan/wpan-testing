3540b1c614474a5ef670eb4b45d83b7c6b68b264
Stefan Schmidt <stefan@osg.samsung.com>
2016-01-20 17:16:57 +0100

    6lowpan: switch address to link-local which allows us to be used without setup
    Instead of a manually added IPv6 address we go with the link-local one. We need
    to specify the interface for ping6 in this case.

--- 6lowpan/ping6.sh
+++ 6lowpan/ping6.sh
@@ -3,7 +3,7 @@
 DEST=$1
 
 # Configuration
-DEST_IPV6=2001:db8:dead:beef::$DEST
+DEST_IPV6=fe80::dcad:beef:babe:$DEST
 COUNT=10
 
 ping6_testing()
@@ -14,25 +14,25 @@ ping6_testing()
 	echo "#####     6LoWPAN connection testing     #####"
 
 	# Default ping packet size (56 + 8), one frame transmitted
-	ping6 -c $COUNT $DEST_IPV6
+	ping6 -I lowpan0 -c $COUNT $DEST_IPV6
 	echo "10 - 11 ms average time expected"
 	echo -e "\r"
 
 	# Minimum ping size (8 + 8) to get timing information, one frame transmitted
-	ping6 -c $COUNT -s 8 $DEST_IPV6
+	ping6 -I lowpan0 -c $COUNT -s 8 $DEST_IPV6
 	echo "7 - 8 ms average time expected"
 	echo -e "\r"
 
 	# Maximum size (?? + 8) to fit into one frame, one frame transmitted
-	#ping6 -c $COUNT -s 8 $DEST_IPV6
+	#ping6 -I lowpan0 -c $COUNT -s 8 $DEST_IPV6
 
 	# Minimum size (?? + 8) to need two frames, two frames transmitted
-	#ping6 -c $COUNT -s 8 $DEST_IPV6
+	#ping6 -I lowpan0 -c $COUNT -s 8 $DEST_IPV6
 
-	ping6 -c $COUNT -s 2000 $DEST_IPV6
+	ping6 -I lowpan0 -c $COUNT -s 2000 $DEST_IPV6
 	echo "230 - 240 ms average time expected"
 
-	#ping6 -c $COUNT -s 8000 $DEST_IPV6
+	#ping6 -I lowpan0 -c $COUNT -s 8000 $DEST_IPV6
 	#echo "920 - 930 ms average time expected"
 }
 
